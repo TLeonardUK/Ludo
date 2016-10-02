83c8af3e9463d8bd476977ae41e4a31af281e1e6
TLeonardUK <tim@programmingbytim.com>
2016-10-02 09:28:12 +0100

    Removed old clang code.

--- Engine/Source/ThirdParty/Clang_/Sync_Clang.bat
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/llvm-mirror/llvm.git
-cd llvm
-git pull origin master
-cd tools
-git clone https://github.com/llvm-mirror/clang.git
-cd clang
-git pull origin master
-cd ../../..
