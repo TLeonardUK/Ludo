66478db1a359abf1b8cabf2bd699fe0f78a5aebe
Tim Leonard <tim@programmingbytim.com>
2017-09-21 16:59:21 +0100

    Update MemoryArena.h

--- Engine/Source/Engine/Core/Public/Memory/MemoryArena.h
+++ Engine/Source/Engine/Core/Public/Memory/MemoryArena.h
@@ -19,52 +19,59 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace Ludo {
 
-/// \brief TODO
+/** \brief A memory arena represents a pool of memory from which individual allocations can be made.
+ *
+ * The primary purpose of arenas is to monitor and group together allocations with share a contextual or 
+ * practical similarity. For example you could group all short lived strings into a small single arena,  all long
+ * lived textures into a large single arena, and so forth.
+ */
 class IMemoryArena
 {
 public:
     
-    /** \brief TODO
+    /** \brief Returns the name of this arena. This should generally be something that describes what it contains.
      * 
-     *  \returns
+     *  \returns Name of this arena.
      */
-	virtual const char* GetName() const = 0;
+    virtual const char* GetName() const = 0;
     
-    /** \brief TODO
+    /** \brief Returns a description of what is contained in this arena. 
      * 
-     *  \returns
+     *  \returns Description of this arena.
      */
-	virtual const char* GetDescription() const = 0;
+    virtual const char* GetDescription() const = 0;
     
-    /** \brief TODO
+    /** \brief Gets the default properties this arena is initialized to on startup 
+     *         if not overridden by platform configuration.
      * 
-     *  \param bResizable
-     *  \param DefaultSize
+     *  \param bResizable  If true this arena can grow when it gets full, otherwise it fails when full.
+     *  \param DefaultSize The default size in bytes that this arena gets initialized to.
      */
-	virtual void GetDefaultState(bool& bResizable, int& DefaultSize) const = 0;
+    virtual void GetDefaultState(bool& bResizable, int& DefaultSize) const = 0;
     
-    /** \brief TODO
+    /** \brief Allocates a block of memory of the given size.
      * 
-     *  \param Count
+     *  \param Count Size in bytes of the block of memory to allocate.
      *
-     *  \returns
+     *  \returns Pointer to allocated block of memory, or nullptr on failure.
      */
-	virtual void* Allocate(const int Count) = 0;
+    virtual void* Allocate(const int Count) = 0;
     
-    /** \brief TODO
+    /** \brief Attempts to reallocate a block of memory previously allocated by this arenas Allocate method.
      * 
-     *  \param Ptr
-     *  \param Count
+     *  \param Ptr   Pointer to previously allocated memory block.
+     *  \param Count New size to resize memory block to.
      *
-     *  \returns
+     *  \returns Pointer to resized memory block, may be the same as the input pointer. Returns
+     *           nullptr on failure.
      */
-	virtual void* Reallocate(void* Ptr, const int Count) = 0;
+    virtual void* Reallocate(void* Ptr, const int Count) = 0;
     
-    /** \brief TODO
+    /** \brief Frees a block of memory previously allocated and returns it to the pool for future use.
      * 
-     *  \param Ptr
+     *  \param Ptr Pointer to block of memory to free.
      */
-	virtual void Free(void* Ptr) = 0;
+    virtual void Free(void* Ptr) = 0;
 
 };
 
